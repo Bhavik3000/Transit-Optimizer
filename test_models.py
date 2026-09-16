bc79885a38837e4effada6af4e0f62b079db0962
Bhavik3000 <142805500+Bhavik3000@users.noreply.github.com>
2026-09-15 18:46:17 -0700

    Update API in test_models.py

--- test_models.py
+++ test_models.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyAn5CTruGqWrtlZilcFs6JNwVvsXFEYF2g")
+client = genai.Client(api_key="GEMINIAI_API_KEY")
 
 print("Here are the models you can use:")
 for model in client.models.list():
